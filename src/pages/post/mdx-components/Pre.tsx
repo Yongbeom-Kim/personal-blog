077e611da306bc9e2a10c379a3c03b7131059fd2
Kim Yongbeom <dernbu@gmail.com>
2025-10-19 19:03:02 +0800

    feat: adjust code font size for mobile

--- src/pages/post/mdx-components/Pre.tsx
+++ src/pages/post/mdx-components/Pre.tsx
@@ -1,5 +1,12 @@
 export function Pre(props: React.HTMLAttributes<HTMLPreElement>) {
+    const {children, ...rest} = props;
     return (
-        <pre {...props} className="bg-gray-400/10 text-sm leading-[1.7] py-4 px-0 my-6 bg-border rounded-md overflow-x-auto"></pre>
+        <pre {...rest} className="bg-gray-400/10 leading-[1.7] py-4 px-0 my-6 bg-border rounded-md overflow-x-auto">
+            <CodeInPre {...(children as React.ReactElement<React.ComponentProps<'code'>>).props} />
+        </pre>
     )
+}
+
+function CodeInPre(props: React.ComponentProps<'code'>) {
+    return <code {...props} className="text-xs sm:text-sm block leading-[1.55]" />;
 }
